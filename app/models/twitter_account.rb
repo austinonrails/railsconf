0945839c3d85e071a7491ab8dedb3cd932e5f071
Chap Ambrose <chapambrose@gmail.com>
2012-04-10 20:58:33 -0500

    put twitter consumer key and secret as env variables

--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -1,12 +1,10 @@
 class TwitterAccount < ActiveRecord::Base
-  CONSUMER_KEY = 'NoWlAAFmzK8FQ52yqs3SQ'
-  CONSUMER_SECRET = 'MeHoZVIgzYeOUihzMsg0jbsxa1EbtzMnc382hupPazw'
   OPTIONS = {:site => "https://api.twitter.com", :request_endpoint => "https://api.twitter.com", :scheme => :header}
 
   def authorize_url(callback_url)
     if self.oauth_authorize_url.blank?
       # Step one, generate a request URL with a request token and secret
-      signing_consumer = OAuth::Consumer.new(TwitterAccount::CONSUMER_KEY, TwitterAccount::CONSUMER_SECRET, TwitterAccount::OPTIONS)
+      signing_consumer = OAuth::Consumer.new(ENV["TWITTER_CONSUMER_KEY"], ENV["TWITTER_CONSUMER_SECRET"], TwitterAccount::OPTIONS)
       request_token = signing_consumer.get_request_token(:oauth_callback => callback_url)
       self.oauth_token = request_token.token
       self.oauth_token_secret = request_token.secret
@@ -18,7 +16,7 @@ class TwitterAccount < ActiveRecord::Base
 
   def validate_oauth_token(oauth_verifier, callback_url)
     begin
-      signing_consumer = OAuth::Consumer.new(TwitterAccount::CONSUMER_KEY, TwitterAccount::CONSUMER_SECRET, TwitterAccount::OPTIONS)
+      signing_consumer = OAuth::Consumer.new(ENV["TWITTER_CONSUMER_KEY"], ENV["TWITTER_CONSUMER_SECRET"], TwitterAccount::OPTIONS)
       access_token = OAuth::RequestToken.new(signing_consumer, self.oauth_token, self.oauth_token_secret).
                                          get_access_token(:oauth_verifier => oauth_verifier)
       self.oauth_token = access_token.params[:oauth_token]
@@ -42,8 +40,8 @@ class TwitterAccount < ActiveRecord::Base
 
   def post(message)
     Twitter.configure do |config|
-      config.consumer_key = TwitterAccount::CONSUMER_KEY
-      config.consumer_secret = TwitterAccount::CONSUMER_SECRET
+      config.consumer_key = ENV["TWITTER_CONSUMER_KEY"]
+      config.consumer_secret = ENV["TWITTER_CONSUMER_SECRET"]
       config.oauth_token = self.oauth_token
       config.oauth_token_secret = self.oauth_token_secret
     end
